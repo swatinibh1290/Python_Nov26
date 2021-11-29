fbf8273c82ebaf7029f419e6e45ba0fe314b25b1
Sylviacccai <xiaoqi.cai1@ucdconnect.ie>
2021-11-29 22:00:47 +0000

    Update Ticker_plotV1.py

--- Ticker_plotV1.py
+++ Ticker_plotV1.py
@@ -9,7 +9,7 @@ import math
 import matplotlib.pyplot as plt
 from Input_ValidationV1 import input_graphvalue
 
-'Import the data from the API abd converts it into csv'
+# Import the data from the API abd converts it into csv
 
 def export_data(symbol, start, end):
     # Download stock data then export as CSV
@@ -17,35 +17,27 @@ def export_data(symbol, start, end):
     data_df.to_csv(symbol.lower() + '.csv')
     return data_df
 
-'Gets the data from API and displays the company information'
 
-def get_company_details(symbol):
-    flag = False
-    while(flag != True):
-    #get company information
-        company_details = yf.Ticker(symbol)
-        hist = company_details.history(period="1mo")
-        if(hist.empty):
-            print("Invalid Symbol")
-            symbol = input("Please enter the valid input symbol: ")
-        else:
-            flag = True     
-    if "longName" and "longBusinessSummary" and "website" in company_details.info:
-        print("\nCompany Name: ", str(company_details.info["longName"]))
-        print("\nCompany Description:", str(company_details.info["longBusinessSummary"]))
-        print("\nCompany Website:",str(company_details.info["website"]))
-    return symbol
-
-'Gets the data from API and plot the history values and also displays the Descriptive statistics like mean, median'
+# Gets the data from API and plot the history values and also displays the Descriptive statistics like mean, median
 
 def get_ticker_data(symbol, period):
+    # create a ticker object
     company_details = yf.Ticker(symbol)
+    
     # get historical market data
     hist = company_details.history(period=period)
     print(hist)
+    
+    # get which type of price you want
     graph_value= input_graphvalue()
+    
+    # plot the price
     hist[graph_value].plot(figsize=(16, 9))
+    # '{} history data for {}'.format(symbol,period)
+    plt.legend(title = '{} history data for {}'.format(symbol,period))
     plt.show()
+    
+    # get mean/ variance/ range/ median/ coefficient of variation value
     print("\nPlease find the Descriptive measures of the value " + graph_value.upper()+" selected:\n")
     mean = hist[graph_value].mean()
     variance = hist[graph_value].var()
@@ -61,12 +53,11 @@ def get_ticker_data(symbol, period):
     print("Co-efficient of variation :" + str(variation_coef))
     return hist 
 
-'Feteches the data from API and return the dataframe to the calling function'
+# Feteches the data from API and return the dataframe to the calling function'
 
 def get_stock_data(symbol, period):
     company_details = yf.Ticker(symbol)
     # get historical market data
     hist = company_details.history(period=period)
     return hist
-
 
